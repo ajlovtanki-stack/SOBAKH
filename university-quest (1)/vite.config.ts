66c482600a90d708a3d42bd79d9b5a6cc9f27a52
ajlovtanki-stack <ajlovtanki@gmail.com>
2026-06-26 20:21:12 +0300

    Update vite.config.ts

--- university-quest (1)/vite.config.ts
+++ university-quest (1)/vite.config.ts
@@ -206,6 +206,7 @@ function vitePluginStorageProxy(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector(), vitePluginStorageProxy()];
 
 export default defineConfig({
+  base: '/university-quest/', // <--- Add this line
   plugins,
   resolve: {
     alias: {
